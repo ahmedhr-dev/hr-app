1efb3155bbeca98139ae17ef4a82a183a96f3e8a
ahmedhr-dev <ahmed.hr@alkuzama.com>
2026-05-05 11:57:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-self.addEventListener("install", e=>{
- self.skipWaiting();
+const CACHE_NAME = "hr-app-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(["./", "./index.html"]);
+    })
+  );
 });
 
-self.addEventListener("activate", e=>{
- e.waitUntil(
-  caches.keys().then(keys =>
-    Promise.all(keys.map(k => caches.delete(k)))
-  )
- );
-});
-
-self.addEventListener("fetch", e=>{
- e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
